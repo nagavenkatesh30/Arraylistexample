68f16d9c56d30006de848e9fe597f836ce64ed6c
nagav <nagav@LAPTOP-UUVCC6JF>
2022-04-24 13:16:18 +0530

    arraylist example

---
+++ src/arraylistexample.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class arraylistexample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ArrayList<String> a=new ArrayList<String>();
+a.add("venkatesh");
+a.add("sunil");
+a.add("aditya");
+a.add("vijay");
+System.out.println(a);
+a.add(0, "sudheer");
+
+System.out.println(a);
+
+a.remove(4);
+System.out.println(a);
+a.remove("sudheer");
+System.out.println(a);
+
+System.out.println(a.get(2));
+System.out.println(a.contains("aditya"));
+
+	}
+
+}
